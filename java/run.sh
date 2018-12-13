e6d2c3a7a76f14506e791bbd80119f093c9b05cf
Braeton Buckley <bbuck001@ucr.edu>
2018-12-13 13:25:32 -0800

    fixed run.sh
    dbname, port, and user should equal 1, 2, 3 respectively
    when running the program, enter the dbname, port, and user manually as shown in the example comment
    ex: source ./run.sh bbuck001_DB 2063 bbuck001

--- java/run.sh
+++ java/run.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
-DBNAME=$ryuzu001_DB
-PORT=$9998
-USER=$ryuzu001
+DBNAME=$1
+PORT=$2
+USER=$3
 
 # Example: source ./run.sh flightDB 5432 user
 java -cp lib/*:bin/ MechanicShop $DBNAME $PORT $USER
